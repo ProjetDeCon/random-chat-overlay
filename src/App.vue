82fd39aac1fc5164d57b2e137894a61edbd8b63b
William Traoré <hi@wil-liam.com>
2025-09-18 21:05:58 +0200

    feat(chat): get all messages from streamers

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import tmi from "tmi.js";
+import { streamers } from "./utils/streamers";
+import { onMounted, ref } from "vue";
+
+const messages = ref<{ username: string; message: string }[]>([]);
+
+onMounted(() => {
+  const client = new tmi.Client({
+    channels: streamers,
+  });
+
+  client.connect();
+
+  client.on("message", (channels, tags, message, self) => {
+    if (self) return;
+
+    messages.value.push({
+      username: tags["display-name"] || tags.username || "",
+      message,
+    });
+    if (messages.value.length > 50) {
+      messages.value.shift();
+    }
+  });
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div v-for="message in messages">
+      {{ message.username }} - {{ message.message }}
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
