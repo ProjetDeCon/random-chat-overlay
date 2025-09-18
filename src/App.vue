11c2e2561402e1f8373c825257bdca02982c588b
William Traoré <hi@wil-liam.com>
2025-09-18 21:53:41 +0200

    feat: handle emotes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import tmi from "tmi.js";
+import tmi from "@tmi.js/chat";
 import { streamers } from "./utils/streamers";
 import { onMounted, ref } from "vue";
 
-const MESSAGE_SPEED = 7;
+const MESSAGE_SPEED = 3;
 
 const messages = ref<{ username: string; message: string; color: string }[]>(
   []
@@ -17,16 +17,31 @@ onMounted(() => {
 
   client.connect();
 
-  client.on("message", (_, tags, message, self) => {
-    if (self) return;
+  client.on("message", (e) => {
+    const { user, message } = e;
 
     counter.value++;
 
     if (counter.value === MESSAGE_SPEED) {
+      const emotes = message.emotes;
+      let msg = message.text;
+
+      emotes.forEach((emote) => {
+        const url = `https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/3.0`;
+        emote.indices.forEach((pos) => {
+          const [start, end] = pos;
+          const toReplace = message.text.slice(start, end + 1);
+          msg = msg.replace(
+            toReplace,
+            `<img class="inline h-6 w-6" src="${url}" />`
+          );
+        });
+      });
+
       messages.value.push({
-        username: tags["display-name"] || tags.username || "",
-        message,
-        color: tags.color || "#ff0000",
+        username: user.display,
+        message: msg,
+        color: user.color,
       });
       if (messages.value.length > 100) {
         messages.value.shift();
@@ -36,6 +51,33 @@ onMounted(() => {
     }
   });
 });
+
+// onMounted(() => {
+//   const client = new tmi.Client({
+//     channels: streamers,
+//   });
+
+//   client.connect();
+
+//   client.on("message", (_, tags, message, self) => {
+//     if (self) return;
+
+//     counter.value++;
+
+//     if (counter.value === MESSAGE_SPEED) {
+//       messages.value.push({
+//         username: tags["display-name"] || tags.username || "",
+//         message,
+//         color: tags.color || "#ff0000",
+//       });
+//       if (messages.value.length > 100) {
+//         messages.value.shift();
+//       }
+
+//       counter.value = 0;
+//     }
+//   });
+// });
 </script>
 
 <template>
@@ -46,10 +88,10 @@ onMounted(() => {
       v-for="message in messages"
       class="font-medium text-white text-shadow-md"
     >
-      <span class="font-bold" :style="{ color: message.color }">{{
+      <span class="font-bold mr-2" :style="{ color: message.color }">{{
         message.username
       }}</span>
-      {{ message.message }}
+      <span v-html="message.message"></span>
     </div>
   </div>
 </template>
